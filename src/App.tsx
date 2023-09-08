b8f4a70f8dc3b03badaacb8f469a6c68bd1ae0f2
enoqueJonas <enoque.macanda@gmail.com>
2023-09-08 16:15:51 +0200

    ADD state to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const handleClick = () => {
+
+  } 
+
   return (
     <div className="App">
-      
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
