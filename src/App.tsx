948d7c1042ac658d09efbe44dd690d80e1e0e3db
enoqueJonas <enoque.macanda@gmail.com>
2023-10-02 19:33:26 +0200

    REFACTOR bundle logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
-import { useState, useEffect, useRef } from "react";
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { useState  } from "react";
 import CodeEditor from "./components/CodeEditor";
 import Preview from "./components/Preview";
+import bundler from "./bundler";
 
 function App() {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
-  const ref = useRef<any>();
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: '/esbuild.wasm'
-    })
-  }
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   const handleClick = async () => {
-    if(!ref.current) return;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    });
-    setCode(result.outputFiles[0].text);
+    const output = await bundler(input)
+    setCode(output);
   } 
 
   return (
